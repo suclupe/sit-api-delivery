4a63a332b8c579dcc349ac6340deeac334f25d51
Suclupe <jsuclupev1@upao.edu.pe>
2023-04-18 14:25:01 -0500

    feat: Crear la entidad food con las propiedades de name, description y price

--- api-delivery/src/main/java/com/delivery/apidelivery/entities/Food.java
+++ api-delivery/src/main/java/com/delivery/apidelivery/entities/Food.java
@@ -1,2 +1,30 @@
-package com.delivery.apidelivery.entities;public class Food {
+package com.delivery.apidelivery.entities;
+
+public class Food {
+    private String name;
+    private String description;
+    private double price;
+    public Food(String name, String description, double price) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public double getPrice() {
+        return price;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
---
+++ api-delivery/src/test/java/com/delivery/apidelivery/ApiDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.delivery.apidelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDeliveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
